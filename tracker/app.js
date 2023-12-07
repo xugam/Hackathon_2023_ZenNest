6337239ae602625a21db36cff19300c12e6f8fe2
xugam <146755084+xugam@users.noreply.github.com>
2023-12-07 07:06:03 +0000

    TrackerPartDone

---
+++ tracker/app.js
@@ -0,0 +1,24 @@
+function calculatePeriod() {
+  const lastPeriod = document.getElementById("lastPeriodDate");
+  const lastPeriodDate = new Date(lastPeriod.value);
+  const cycleLength = 28;
+
+  const nextPeriodDate = new Date(
+    lastPeriodDate.getTime() + cycleLength * 24 * 60 * 60 * 1000
+  );
+
+  const currentDate = new Date();
+  const timeRemaining = nextPeriodDate.getTime() - currentDate.getTime();
+
+  const daysRemaining = Math.ceil(timeRemaining / (24 * 60 * 60 * 1000));
+
+  if (daysRemaining < 0) {
+    const final = document.getElementById("Your_Result");
+    final.innerHTML = `Invalid `;
+    return;
+  } else {
+    const final = document.getElementById("Your_Result");
+    final.innerHTML = `Your next period is expected around ${nextPeriodDate.toDateString()}.<br>Time remaining: ${daysRemaining} days.
+  <br><br> Please note that the accuracy of your data may be affected, as our period tracker is designed to accommodate cycles with intervals of either 23 or 28 days.`;
+  }
+}
